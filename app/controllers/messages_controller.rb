916c53b60af28ab0315e3a21bc949021c673d707
wadagaku <obusumagaku.1005@gmail.com>
2015-09-01 08:58:44 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
   def index
+    # Messageを全て取得する。
+    @messages = Message.all
+  end
+
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  private
+  def message_params
+    # params[:message]のパラメータで name , bodyのみを許可する。
+    # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
+    params.require(:message).permit(:name, :body)
   end
 end
